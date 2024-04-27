f69acec4e8d35ebd5b3fbe04a1f3ee6a1eb3be49
bctran <bao.cong.tran@mgm-tp.com>
2024-04-27 17:27:27 +0700

    Refactor DB

--- src/login/jsonRpcMethodHandlers.ts
+++ src/login/jsonRpcMethodHandlers.ts
@@ -2,7 +2,6 @@ import "scope-extensions-js";
 import {JSONRPC, JSONRPCID, JSONRPCRequest, JSONRPCResponse, JSONRPCServer} from "json-rpc-2.0";
 import endeavorDB, {Admin, Teacher} from "../databases/endeavorDB";
 import {generateJWT} from "../jwt/jwt";
-import {NoResultError} from "kysely";
 
 export default new JSONRPCServer().apply(function () {
     this.addMethodAdvanced("login", login);
@@ -24,38 +23,39 @@ function login(jsonRPCRequest: JSONRPCRequest): Promise<JSONRPCResponse> {
     }
 
     return queryUserFromDB(userType, jsonRPCRequest.params.username, jsonRPCRequest.params.password)
-        .then((user) => {
-            const {password, ...userInfo} = user; // Remove the password field
+        .then((users) => {
+                if (users.length) {
+                    const {password, ...userInfo} = users[0]; // Remove the password field
+                    return {
+                        jsonrpc: JSONRPC,
+                        id: jsonRPCRequest.id as JSONRPCID,
+                        result: {jwt: generateJWT({userType: userType, ...userInfo})},
+                    };
+                } else {
+                    console.info("Invalid username or password:", jsonRPCRequest.params);
+                    return {
+                        jsonrpc: JSONRPC,
+                        id: jsonRPCRequest.id as JSONRPCID,
+                        error: {
+                            code: -100,
+                            message: "Invalid username or password.",
+                            data: jsonRPCRequest.params,
+                        },
+                    };
+                }
+            }
+        )
+        .catch((error) => {
+            console.error("An unexpected error occurred:", error);
             return {
                 jsonrpc: JSONRPC,
                 id: jsonRPCRequest.id as JSONRPCID,
-                result: {jwt: generateJWT({userType: userType, ...userInfo})},
+                error: {
+                    code: -100,
+                    message: `An unexpected error occurred: ${error}`,
+                    data: jsonRPCRequest.params,
+                },
             };
-        })
-        .catch((error) => {
-            if (error instanceof NoResultError) {
-                console.info("Invalid username or password:", jsonRPCRequest.params);
-                return {
-                    jsonrpc: JSONRPC,
-                    id: jsonRPCRequest.id as JSONRPCID,
-                    error: {
-                        code: -100,
-                        message: "Invalid username or password.",
-                        data: jsonRPCRequest.params,
-                    },
-                };
-            } else {
-                console.error("An unexpected error occurred:", error);
-                return {
-                    jsonrpc: JSONRPC,
-                    id: jsonRPCRequest.id as JSONRPCID,
-                    error: {
-                        code: -100,
-                        message: `An unexpected error occurred: ${error}`,
-                        data: jsonRPCRequest.params,
-                    },
-                };
-            }
         });
 }
 
@@ -63,12 +63,12 @@ function isUserType(userType: any): userType is UserType {
     return ["admin", "teacher", "student"].includes(userType)
 }
 
-function queryUserFromDB(userTable: UserType, username: string, password: string): Promise<Admin | Teacher> {
+function queryUserFromDB(userTable: UserType, username: string, password: string): Promise<Admin[] | Teacher[]> {
     return endeavorDB.selectFrom<UserType>(userTable)
         .selectAll()
         .where("username", "=", username)
         .where("password", "=", password)
-        .executeTakeFirstOrThrow()
+        .execute()
 }
 
 type UserType = "admin" | "teacher" | "student"
