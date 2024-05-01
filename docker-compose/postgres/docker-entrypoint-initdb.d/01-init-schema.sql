d770d41708a55e56722d216ffbc53d544c8a9d29
bctran <bao.cong.tran@mgm-tp.com>
2024-05-01 09:42:38 +0700

    teach createCard

--- docker-compose/postgres/docker-entrypoint-initdb.d/01-init-schema.sql
+++ docker-compose/postgres/docker-entrypoint-initdb.d/01-init-schema.sql
@@ -15,30 +15,30 @@ CREATE TABLE ADMIN
 -- Define table structure for teachers
 CREATE TABLE TEACHER
 (
-    username      VARCHAR(255) PRIMARY KEY, -- Unique identifier for the teacher
-    password      VARCHAR(255) NOT NULL,    -- Password for the teacher
-    surname       VARCHAR(255) NOT NULL,    -- Surname of the teacher
-    given_name    VARCHAR(255) NOT NULL,    -- Given name of the teacher
-    email         VARCHAR(255) NOT NULL,    -- Email of the teacher
-    phone         VARCHAR(255) NOT NULL,    -- Phone number of the teacher
-    date_of_birth DATE         NOT NULL,    -- Date of birth of the teacher
-    address       TEXT         NOT NULL,    -- Address of the teacher
-    avatar        VARCHAR(255)              -- URL/path to the teacher's avatar
+    username      VARCHAR(255) PRIMARY KEY,             -- Unique identifier for the teacher
+    password      VARCHAR(255) NOT NULL,                -- Password for the teacher
+    surname       VARCHAR(255) NOT NULL,                -- Surname of the teacher
+    given_name    VARCHAR(255) NOT NULL,                -- Given name of the teacher
+    email         VARCHAR(255) NOT NULL,                -- Email of the teacher
+    phone         VARCHAR(255) NOT NULL,                -- Phone number of the teacher
+    date_of_birth DATE         NOT NULL,                -- Date of birth of the teacher
+    address       TEXT         NOT NULL,                -- Address of the teacher
+    avatar        VARCHAR(255)                          -- URL/path to the teacher's avatar
 );
 
 -- Define table structure for students
 CREATE TABLE STUDENT
 (
-    username      VARCHAR(255) PRIMARY KEY, -- Unique identifier for the student
-    password      VARCHAR(255) NOT NULL,    -- Password for the student
-    surname       VARCHAR(255) NOT NULL,    -- Surname of the student
-    given_name    VARCHAR(255) NOT NULL,    -- Given name of the student
-    email         VARCHAR(255),             -- Email of the student
-    phone         VARCHAR(255),             -- Phone number of the student
-    date_of_birth DATE         NOT NULL,    -- Date of birth of the student
-    address       TEXT         NOT NULL,    -- Address of the student
-    avatar        VARCHAR(255),             -- URL/path to the student's avatar
-    proficiency   INTEGER                   -- Proficiency level of the student
+    username      VARCHAR(255) PRIMARY KEY,             -- Unique identifier for the student
+    password      VARCHAR(255) NOT NULL,                -- Password for the student
+    surname       VARCHAR(255) NOT NULL,                -- Surname of the student
+    given_name    VARCHAR(255) NOT NULL,                -- Given name of the student
+    email         VARCHAR(255),                         -- Email of the student
+    phone         VARCHAR(255),                         -- Phone number of the student
+    date_of_birth DATE         NOT NULL,                -- Date of birth of the student
+    address       TEXT         NOT NULL,                -- Address of the student
+    avatar        VARCHAR(255),                         -- URL/path to the student's avatar
+    proficiency   INTEGER                               -- Proficiency level of the student
 );
 
 -- Define custom enumeration type for course status
@@ -46,14 +46,14 @@ CREATE TYPE COURSE_STATUS AS ENUM ('DRAFT', 'IN_REVIEW', 'APPROVED', 'PUBLISHED'
 -- Define table structure for courses
 CREATE TABLE COURSE
 (
-    id          SERIAL PRIMARY KEY,                 -- Unique identifier for the course
-    level       INT          NOT NULL,              -- Level of the course
-    title       VARCHAR(255) NOT NULL,              -- Title of the course
-    status      COURSE_STATUS,                      -- Status of the course
-    summary     VARCHAR(255),                       -- Summary of the course
-    description TEXT,                               -- Detailed description of the course
-    thumbnail   VARCHAR(255),                       -- URL/path to the course thumbnail
-    updated_at  timestamp default current_timestamp,-- Timestamp of the last update
+    id          SERIAL PRIMARY KEY,                     -- Unique identifier for the course
+    level       INT          NOT NULL,                  -- Level of the course
+    title       VARCHAR(255) NOT NULL,                  -- Title of the course
+    status      COURSE_STATUS,                          -- Status of the course
+    summary     VARCHAR(255),                           -- Summary of the course
+    description TEXT,                                   -- Detailed description of the course
+    thumbnail   VARCHAR(255),                           -- URL/path to the course thumbnail
+    updated_at  timestamp default current_timestamp,    -- Timestamp of the last update
     CONSTRAINT unique_status_title_level UNIQUE (status, title, level)
 );
 
@@ -82,29 +82,29 @@ CREATE TABLE LESSON
 -- Table definition for CARD
 CREATE TABLE CARD
 (
-    id              INTEGER PRIMARY KEY,           -- Unique identifier for the card
-    lesson_id       INTEGER REFERENCES LESSON (id),-- Foreign key referencing lesson
-    front_text      TEXT,                          -- Text on the front side of the card
-    front_audio_uri TEXT                           -- URI for audio associated with the front side
+    id              INTEGER PRIMARY KEY,                -- Unique identifier for the card
+    lesson_id       INTEGER REFERENCES LESSON (id),     -- Foreign key referencing lesson
+    front_text      TEXT,                               -- Text on the front side of the card
+    front_audio_uri TEXT                                -- URI for audio associated with the front side
 );
 
 -- Table definition for WORD
 CREATE TABLE WORD
 (
-    id              INTEGER         PRIMARY KEY,    -- Unique identifier for the word
-    word            VARCHAR(255)    NOT NULL,       -- The word itself
-    definition      TEXT            NOT NULL,       -- Definition of the word
-    phonetic        VARCHAR(255)    NOT NULL,       -- Phonetic pronunciation of the word
-    part_of_speech  VARCHAR(255)    NOT NULL,       -- Part of speech of the word
-    audio_uri       TEXT,                           -- URI for audio associated with the word
-    image_uri       TEXT                            -- URI for image associated with the word
+    id              INTEGER         PRIMARY KEY,        -- Unique identifier for the word
+    word            VARCHAR(255)    NOT NULL,           -- The word itself
+    definition      TEXT            NOT NULL,           -- Definition of the word
+    phonetic        VARCHAR(255)    NOT NULL,           -- Phonetic pronunciation of the word
+    part_of_speech  VARCHAR(255)    NOT NULL,           -- Part of speech of the word
+    audio_uri       TEXT,                               -- URI for audio associated with the word
+    image_uri       TEXT                                -- URI for image associated with the word
 );
 
 -- Table definition for CARD_WORD
 CREATE TABLE CARD_WORD
 (
-    card_id         INTEGER REFERENCES CARD (id),   -- Foreign key referencing card
-    word_id         INTEGER REFERENCES WORD (id),   -- Foreign key referencing word
-    word_order      INTEGER,                        -- Relative order of the word in the card
-    PRIMARY KEY (card_id, word_id)                  -- Composite primary key
+    card_id         INTEGER REFERENCES CARD (id),       -- Foreign key referencing card
+    word_id         INTEGER REFERENCES WORD (id),       -- Foreign key referencing word
+    word_order      INTEGER,                            -- Relative order of the word in the card
+    PRIMARY KEY (card_id, word_id)                      -- Composite primary key
 );
