20edeb51827a84edcd8a00044911fb69e621f993
bctran <bao.cong.tran@mgm-tp.com>
2024-05-01 10:04:23 +0700

    teach createWord

--- src/teach/wordMethods.ts
+++ src/teach/wordMethods.ts
@@ -2,22 +2,35 @@ import "scope-extensions-js";
 import {endeavorDB} from "../databases/endeavorDB";
 import {Word} from "../databases/endeavorDB";
 import {Insertable, Updateable} from "kysely";
+import {Schema} from "express-validator";
 
-export function createWord(word: Insertable<Word>) {
-    return endeavorDB.insertInto("word").values(word).returningAll().executeTakeFirstOrThrow();
+export {wordRpcParamsSchemas, createWord}
+
+function createWord(request: any, response: any) {
+    return endeavorDB
+        .insertInto("word")
+        .values(request.body.params)
+        .returningAll()
+        .executeTakeFirstOrThrow();
 }
 
-export function readWord({id}: { id: number }) {
+function readWord({id}: { id: number }) {
     return endeavorDB.selectFrom("word").selectAll().where("id", "=", id).executeTakeFirstOrThrow();
 }
 
-export function updateWord(word: Updateable<Word>) {
+function updateWord(word: Updateable<Word>) {
     return endeavorDB.updateTable("word").where("id", "=", word.id!!).set(word).returningAll().executeTakeFirstOrThrow();
 }
 
-export function deleteWord({id}: { id: number }) {
+function deleteWord({id}: { id: number }) {
     return endeavorDB.deleteFrom("word").where("id", "=", id).returningAll().executeTakeFirstOrThrow();
 }
 
-export function findWord() {
+function findWord() {
 }
+
+type RpcMethodNames = "createWord";
+
+const wordRpcParamsSchemas: Record<RpcMethodNames, Schema> = {
+    "createWord": {}
+};
