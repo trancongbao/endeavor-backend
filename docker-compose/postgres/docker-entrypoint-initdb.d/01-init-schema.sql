8d979fb00f2a84647215f025464873682af68df0
bctran <bao.cong.tran@mgm-tp.com>
2024-05-01 09:38:57 +0700

    teach createCard

--- docker-compose/postgres/docker-entrypoint-initdb.d/01-init-schema.sql
+++ docker-compose/postgres/docker-entrypoint-initdb.d/01-init-schema.sql
@@ -59,57 +59,52 @@ CREATE TABLE COURSE
 
 CREATE TABLE TEACHER_COURSE
 (
-    course_id        INT          NOT NULL, -- Id of the course
-    teacher_username VARCHAR(255) NOT NULL, -- Unique identifier of the teacher
-    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES COURSE (id),
-    CONSTRAINT fk_teacher FOREIGN KEY (teacher_username) REFERENCES TEACHER (username),
+    course_id           INT NOT NULL REFERENCES COURSE (id),                   -- Reference to the id column of COURSE table
+    teacher_username    VARCHAR(255) NOT NULL REFERENCES TEACHER (username),   -- Reference to the username column of TEACHER table
     CONSTRAINT unique_teacher_id_course_id UNIQUE (course_id, teacher_username)
 );
 
 CREATE TABLE LESSON
 (
-    id          SERIAL PRIMARY KEY,                 -- Unique identifier for the lesson, auto-incremented
-    course_id   INTEGER REFERENCES COURSE (id),     -- Foreign key referencing the course that the lesson belongs to
-    position    INTEGER      NOT NULL,              -- Order of the lesson within the course, cannot be null
-    title       VARCHAR(255) NOT NULL,              -- Title of the lesson, cannot be null
-    audio       VARCHAR(255) NOT NULL,              -- Path to the audio file for the lesson, cannot be null
-    summary     TEXT,                               -- Summary of the lesson
-    description TEXT,                               -- Description of the lesson
-    thumbnail   VARCHAR(255),                       -- Path to the thumbnail image for the lesson
-    content     TEXT,                               -- Content of the lesson
-    updated_at  timestamp default current_timestamp,-- Timestamp of the last update
-    CONSTRAINT unique_course_id_order UNIQUE (course_id, position)
+    id              SERIAL PRIMARY KEY,                 -- Unique identifier for the lesson, auto-incremented
+    course_id       INTEGER REFERENCES COURSE (id),     -- Foreign key referencing the course that the lesson belongs to
+    lesson_order    INTEGER      NOT NULL,          -- Order of the lesson within the course, cannot be null
+    title           VARCHAR(255) NOT NULL,              -- Title of the lesson, cannot be null
+    audio           VARCHAR(255) NOT NULL,              -- Path to the audio file for the lesson, cannot be null
+    summary         TEXT,                               -- Summary of the lesson
+    description     TEXT,                               -- Description of the lesson
+    thumbnail       VARCHAR(255),                       -- Path to the thumbnail image for the lesson
+    content         TEXT,                               -- Content of the lesson
+    updated_at      timestamp default current_timestamp,-- Timestamp of the last update
+    CONSTRAINT unique_course_id_order UNIQUE (course_id, lesson_order)
 );
 
 -- Table definition for CARD
 CREATE TABLE CARD
 (
     id              INTEGER PRIMARY KEY,           -- Unique identifier for the card
-    lesson_id       INTEGER,                       -- Foreign key referencing lesson
+    lesson_id       INTEGER REFERENCES LESSON (id),-- Foreign key referencing lesson
     front_text      TEXT,                          -- Text on the front side of the card
-    front_audio_uri TEXT,                          -- URI for audio associated with the front side
-    FOREIGN KEY (lesson_id) REFERENCES LESSON (id) -- Reference to lesson table
+    front_audio_uri TEXT                           -- URI for audio associated with the front side
 );
 
 -- Table definition for WORD
 CREATE TABLE WORD
 (
-    id             INTEGER PRIMARY KEY, -- Unique identifier for the word
-    word           TEXT,                -- The word itself
-    definition     TEXT,                -- Definition of the word
-    phonetic       TEXT,                -- Phonetic pronunciation of the word
-    part_of_speech TEXT,                -- Part of speech of the word
-    audio_uri      TEXT,                -- URI for audio associated with the word
-    image_uri      TEXT                 -- URI for image associated with the word
+    id              INTEGER         PRIMARY KEY,    -- Unique identifier for the word
+    word            VARCHAR(255)    NOT NULL,       -- The word itself
+    definition      TEXT            NOT NULL,       -- Definition of the word
+    phonetic        VARCHAR(255)    NOT NULL,       -- Phonetic pronunciation of the word
+    part_of_speech  VARCHAR(255)    NOT NULL,       -- Part of speech of the word
+    audio_uri       TEXT,                           -- URI for audio associated with the word
+    image_uri       TEXT                            -- URI for image associated with the word
 );
 
 -- Table definition for CARD_WORD
 CREATE TABLE CARD_WORD
 (
-    card_id    INTEGER,                         -- Foreign key referencing card
-    word_id    INTEGER,                         -- Foreign key referencing word
-    word_order INTEGER,                         -- Relative order of the word in the card
-    PRIMARY KEY (card_id, word_id),             -- Composite primary key
-    FOREIGN KEY (card_id) REFERENCES CARD (id), -- Reference to card table
-    FOREIGN KEY (word_id) REFERENCES WORD (id)  -- Reference to word table
+    card_id         INTEGER REFERENCES CARD (id),   -- Foreign key referencing card
+    word_id         INTEGER REFERENCES WORD (id),   -- Foreign key referencing word
+    word_order      INTEGER,                        -- Relative order of the word in the card
+    PRIMARY KEY (card_id, word_id)                  -- Composite primary key
 );
