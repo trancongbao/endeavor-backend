7a4cf1554ad973635854bd52ee742b26cd5b8772
bctran <bao.cong.tran@mgm-tp.com>
2024-05-05 16:18:49 +0700

    /auth currentUser

---
+++ src/auth/currentUser.ts
@@ -0,0 +1,75 @@
+import {endeavorDB, Admin, Student, Teacher} from "../databases/endeavorDB";
+import {Codes, sendErrorResponse} from "../response/error";
+import {sendSuccessResponse} from "../response/success";
+
+export {paramsSchema, currentUser};
+
+const paramsSchema = {}
+
+function currentUser(request: any, response: any) {
+    const {userType, username, password} = request.body.params
+
+    if (userType === "admin") {
+        if ((username === process.env.ADMIN_USERNAME) && (password === process.env.ADMIN_PASSWORD)) {
+            const userInfo = {
+                username: username,
+                password: password
+            }
+            /**
+             * Add authenticated session data
+             */
+            request.session.userType = userType
+            request.session.userInfo = userInfo
+            return sendSuccessResponse(response, {
+                userType: userType,
+                userInfo: userInfo
+            })
+        } else {
+            return sendErrorResponse(
+                response,
+                Codes.Auth.Login.InvalidUserNameOrPassword,
+                "Invalid username or password."
+            )
+        }
+    }
+
+    queryUserFromDB(userType, username, password)
+        .then((users) => {
+            if (users.length) {
+                const {password, ...userInfo} = users[0]; // Remove the password field
+                /**
+                 * Add authenticated session data
+                 */
+                request.session.userType = userType
+                request.session.userInfo = userInfo
+
+                sendSuccessResponse(response, {
+                    userType: userType,
+                    userInfo: userInfo
+                })
+            } else {
+                return sendErrorResponse(
+                    response,
+                    Codes.Auth.Login.InvalidUserNameOrPassword,
+                    "Invalid username or password."
+                )
+            }
+        })
+        .catch((error) => {
+            return sendErrorResponse(
+                response,
+                Codes.Auth.Login.UnexpectedError,
+                `An unexpected error occurred: ${error}`,
+            )
+        })
+}
+
+function queryUserFromDB(userTable: UserType, username: string, password: string): Promise<Admin[] | Teacher[] | Student[]> {
+    return endeavorDB.selectFrom<UserType>(userTable)
+        .selectAll()
+        .where("username", "=", username)
+        .where("password", "=", password)
+        .execute()
+}
+
+type UserType = "admin" | "teacher" | "student"
