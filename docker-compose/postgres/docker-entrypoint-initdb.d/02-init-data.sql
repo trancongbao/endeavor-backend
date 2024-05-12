58ad18845c61560e55c13b84817f831c39428ebd
bctran <bao.cong.tran@mgm-tp.com>
2024-05-12 09:25:05 +0700

    docker-entrypoint-initdb.d

--- docker-compose/postgres/docker-entrypoint-initdb.d/02-init-data.sql
+++ docker-compose/postgres/docker-entrypoint-initdb.d/02-init-data.sql
@@ -1,3 +1,146 @@
+/***********************************************************************************************************************
+
+   SSSSSSSSSSSSSSS                     hhhhhhh
+ SS:::::::::::::::S                    h:::::h
+S:::::SSSSSS::::::S                    h:::::h
+S:::::S     SSSSSSS                    h:::::h
+S:::::S                cccccccccccccccc h::::h hhhhh           eeeeeeeeeeee       mmmmmmm    mmmmmmm     aaaaaaaaaaaaa
+S:::::S              cc:::::::::::::::c h::::hh:::::hhh      ee::::::::::::ee   mm:::::::m  m:::::::mm   a::::::::::::a
+ S::::SSSS          c:::::::::::::::::c h::::::::::::::hh   e::::::eeeee:::::eem::::::::::mm::::::::::m  aaaaaaaaa:::::a
+  SS::::::SSSSS    c:::::::cccccc:::::c h:::::::hhh::::::h e::::::e     e:::::em::::::::::::::::::::::m           a::::a
+    SSS::::::::SS  c::::::c     ccccccc h::::::h   h::::::he:::::::eeeee::::::em:::::mmm::::::mmm:::::m    aaaaaaa:::::a
+       SSSSSS::::S c:::::c              h:::::h     h:::::he:::::::::::::::::e m::::m   m::::m   m::::m  aa::::::::::::a
+            S:::::Sc:::::c              h:::::h     h:::::he::::::eeeeeeeeeee  m::::m   m::::m   m::::m a::::aaaa::::::a
+            S:::::Sc::::::c     ccccccc h:::::h     h:::::he:::::::e           m::::m   m::::m   m::::ma::::a    a:::::a
+SSSSSSS     S:::::Sc:::::::cccccc:::::c h:::::h     h:::::he::::::::e          m::::m   m::::m   m::::ma::::a    a:::::a
+S::::::SSSSSS:::::S c:::::::::::::::::c h:::::h     h:::::h e::::::::eeeeeeee  m::::m   m::::m   m::::ma:::::aaaa::::::a
+S:::::::::::::::SS   cc:::::::::::::::c h:::::h     h:::::h  ee:::::::::::::e  m::::m   m::::m   m::::m a::::::::::aa:::a
+ SSSSSSSSSSSSSSS       cccccccccccccccc hhhhhhh     hhhhhhh    eeeeeeeeeeeeee  mmmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaa
+
+Source: https://patorjk.com/software/taag/#p=display&h=0&v=0&f=Doh&t=Data
+***********************************************************************************************************************/
+
+-- Define table structure for teachers
+CREATE TABLE TEACHER
+(
+    username      VARCHAR(255) PRIMARY KEY, -- Unique identifier for the teacher
+    password      VARCHAR(255) NOT NULL,    -- Password for the teacher
+    surname       VARCHAR(255) NOT NULL,    -- Surname of the teacher
+    given_name    VARCHAR(255) NOT NULL,    -- Given name of the teacher
+    email         VARCHAR(255) NOT NULL,    -- Email of the teacher
+    phone         VARCHAR(255) NOT NULL,    -- Phone number of the teacher
+    date_of_birth DATE         NOT NULL,    -- Date of birth of the teacher
+    address       TEXT         NOT NULL,    -- Address of the teacher
+    avatar        VARCHAR(255)              -- URL/path to the teacher's avatar
+);
+
+-- Define table structure for students
+CREATE TABLE STUDENT
+(
+    username      VARCHAR(255) PRIMARY KEY, -- Unique identifier for the student
+    password      VARCHAR(255) NOT NULL,    -- Password for the student
+    surname       VARCHAR(255) NOT NULL,    -- Surname of the student
+    given_name    VARCHAR(255) NOT NULL,    -- Given name of the student
+    email         VARCHAR(255),             -- Email of the student
+    phone         VARCHAR(255),             -- Phone number of the student
+    date_of_birth DATE         NOT NULL,    -- Date of birth of the student
+    address       TEXT         NOT NULL,    -- Address of the student
+    avatar        VARCHAR(255),             -- URL/path to the student's avatar
+    proficiency   INTEGER                   -- Proficiency level of the student
+);
+
+-- Define custom enumeration type for course status
+CREATE TYPE COURSE_STATUS AS ENUM ('DRAFT', 'IN_REVIEW', 'APPROVED', 'PUBLISHED', 'ARCHIVED');
+-- Define table structure for courses
+CREATE TABLE COURSE
+(
+    id          SERIAL PRIMARY KEY,                  -- Unique identifier for the course
+    level       INT          NOT NULL,               -- Level of the course
+    title       VARCHAR(255) NOT NULL,               -- Title of the course
+    status      COURSE_STATUS,                       -- Status of the course
+    summary     VARCHAR(255),                        -- Summary of the course
+    description TEXT,                                -- Detailed description of the course
+    thumbnail   VARCHAR(255),                        -- URL/path to the course thumbnail
+    updated_at  timestamp default current_timestamp, -- Timestamp of the last update
+    CONSTRAINT unique_status_title_level UNIQUE (status, title, level)
+);
+
+CREATE TABLE TEACHER_COURSE
+(
+    course_id        INT          NOT NULL REFERENCES COURSE (id),        -- Reference to the id column of COURSE table
+    teacher_username VARCHAR(255) NOT NULL REFERENCES TEACHER (username), -- Reference to the username column of TEACHER table
+    CONSTRAINT unique_teacher_id_course_id UNIQUE (course_id, teacher_username)
+);
+
+CREATE TABLE LESSON
+(
+    id           SERIAL PRIMARY KEY,                 -- Unique identifier for the lesson, auto-incremented
+    course_id    INTEGER REFERENCES COURSE (id),     -- Foreign key referencing the course that the lesson belongs to
+    lesson_order INTEGER      NOT NULL,              -- Order of the lesson within the course, cannot be null
+    title        VARCHAR(255) NOT NULL,              -- Title of the lesson, cannot be null
+    audio        VARCHAR(255) NOT NULL,              -- Path to the audio file for the lesson, cannot be null
+    summary      TEXT,                               -- Summary of the lesson
+    description  TEXT,                               -- Description of the lesson
+    thumbnail    VARCHAR(255),                       -- Path to the thumbnail image for the lesson
+    content      TEXT,                               -- Content of the lesson
+    updated_at   timestamp default current_timestamp,-- Timestamp of the last update
+    CONSTRAINT unique_course_id_lesson_order UNIQUE (course_id, lesson_order)
+);
+
+-- Table definition for CARD
+CREATE TABLE CARD
+(
+    id              SERIAL PRIMARY KEY,             -- Unique identifier for the card
+    lesson_id       INTEGER REFERENCES LESSON (id), -- Foreign key referencing lesson
+    card_order      INTEGER NOT NULL,               -- Relative order of the word in the card
+    front_text      TEXT    NOT NULL,               -- Text on the front side of the card
+    front_audio_uri TEXT,                           -- URI for audio associated with the front side
+    CONSTRAINT unique_lesson_id_card_order UNIQUE (lesson_id, card_order)
+);
+
+-- Table definition for WORD
+CREATE TABLE WORD
+(
+    id             SERIAL PRIMARY KEY,    -- Unique identifier for the word
+    word           VARCHAR(255) NOT NULL, -- The word itself
+    definition     TEXT         NOT NULL, -- Definition of the word
+    phonetic       VARCHAR(255),          -- Phonetic pronunciation of the word
+    part_of_speech VARCHAR(255),          -- Part of speech of the word
+    audio_uri      TEXT,                  -- URI for audio associated with the word
+    image_uri      TEXT                   -- URI for image associated with the word
+);
+
+-- Table definition for CARD_WORD
+CREATE TABLE CARD_WORD
+(
+    card_id    INTEGER REFERENCES CARD (id), -- Foreign key referencing card
+    word_id    INTEGER REFERENCES WORD (id), -- Foreign key referencing word
+    word_order INTEGER NOT NULL,             -- Relative order of the word in the card
+    PRIMARY KEY (card_id, word_id)           -- Composite primary key
+);
+
+/***********************************************************************************************************************
+
+DDDDDDDDDDDDD                                   tttt
+D::::::::::::DDD                             ttt:::t
+D:::::::::::::::DD                           t:::::t
+DDD:::::DDDDD:::::D                          t:::::t
+  D:::::D    D:::::D   aaaaaaaaaaaaa   ttttttt:::::ttttttt      aaaaaaaaaaaaa
+  D:::::D     D:::::D  a::::::::::::a  t:::::::::::::::::t      a::::::::::::a
+  D:::::D     D:::::D  aaaaaaaaa:::::a t:::::::::::::::::t      aaaaaaaaa:::::a
+  D:::::D     D:::::D           a::::a tttttt:::::::tttttt               a::::a
+  D:::::D     D:::::D    aaaaaaa:::::a       t:::::t              aaaaaaa:::::a
+  D:::::D     D:::::D  aa::::::::::::a       t:::::t            aa::::::::::::a
+  D:::::D     D:::::D a::::aaaa::::::a       t:::::t           a::::aaaa::::::a
+  D:::::D    D:::::D a::::a    a:::::a       t:::::t    tttttta::::a    a:::::a
+DDD:::::DDDDD:::::D  a::::a    a:::::a       t::::::tttt:::::ta::::a    a:::::a
+D:::::::::::::::DD   a:::::aaaa::::::a       tt::::::::::::::ta:::::aaaa::::::a
+D::::::::::::DDD      a::::::::::aa:::a        tt:::::::::::tt a::::::::::aa:::a
+DDDDDDDDDDDDD          aaaaaaaaaa  aaaa          ttttttttttt    aaaaaaaaaa  aaaa
+
+Source: https://patorjk.com/software/taag/#p=display&h=0&v=0&f=Doh&t=Schema
+***********************************************************************************************************************/
+
 -- Seed data for TEACHER table
 INSERT INTO TEACHER (username, password, surname, given_name, email, phone, date_of_birth, address, avatar)
 VALUES ('teacher1', 'password1', 'Garcia', 'Carlos', 'carlos.garcia@example.com', '+1112223333', '1980-07-10',
