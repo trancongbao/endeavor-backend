4f54cc169f62f80f27a0dc66005f41d11b612cb2
trancongbao <96561274+trancongbao@users.noreply.github.com>
2024-05-25 20:57:28 +0700

    /admin refactor (#52)

--- src/admin/admin.ts
+++ src/admin/admin.ts
@@ -1,32 +1,28 @@
-import {courseRpcParamsSchemas, createCourse, assignCourse} from "./courseMethods";
-import {paramsSchema as createTeacherParamsSchema, createTeacher} from "./teacherMethods";
-import {Schema} from 'express-validator'
-import {Codes} from "../response/error";
-import {validate} from "../validation/validation";
+import { RpcMethodName as CourseRpcMethodName, rpcMethods as courseRpcMethods } from "./courseMethods";
+import { RpcMethodName as TeacherRpcMethodName, rpcMethods as teacherRpcMethods } from "./teacherMethods";
+import { Schema } from "express-validator";
+import { Codes } from "../response/error";
+import { validate } from "../validation/validation";
 
-export {rpcMethods, validateParams, admin};
+export { rpcMethods, validateParams, admin };
 
-const rpcMethods: Record<RpcMethodName, { rpcMethod: CallableFunction, rpcMethodParamsSchema: Schema }> = {
-    "createTeacher": {
-        rpcMethod: createTeacher,
-        rpcMethodParamsSchema: createTeacherParamsSchema
-    },
-    "createCourse": {
-        rpcMethod: createCourse,
-        rpcMethodParamsSchema: courseRpcParamsSchemas["createCourse"]
-    },
-    "assignCourse": {
-        rpcMethod: assignCourse,
-        rpcMethodParamsSchema: courseRpcParamsSchemas["assignCourse"]
-    }
-}
+type RpcMethodName = CourseRpcMethodName | TeacherRpcMethodName;
+
+const rpcMethods: Record<RpcMethodName, { rpcMethod: CallableFunction; rpcMethodParamsSchema: Schema }> = {
+  ...courseRpcMethods,
+  ...teacherRpcMethods,
+};
 
 async function validateParams(request: any, response: any, next: any) {
-    await validate(request, response, next, rpcMethods[request.body.method as RpcMethodName].rpcMethodParamsSchema, Codes.Auth.InputValidationError)
+  await validate(
+    request,
+    response,
+    next,
+    rpcMethods[request.body.method as RpcMethodName].rpcMethodParamsSchema,
+    Codes.Auth.InputValidationError
+  );
 }
 
 function admin(request: { body: { method: RpcMethodName } }, response: any) {
-    rpcMethods[request.body.method].rpcMethod(request, response)
+  rpcMethods[request.body.method].rpcMethod(request, response);
 }
-
-type RpcMethodName = "createTeacher" | "createCourse" | "assignCourse";
--- src/admin/teacherMethods.ts
+++ src/admin/teacherMethods.ts
@@ -1,113 +1,122 @@
 import bcrypt from "bcryptjs";
-import {endeavorDB, Teacher} from "../databases/endeavorDB";
-import {Insertable, Updateable} from "kysely";
-import {Schema} from "express-validator";
+import { endeavorDB, Teacher } from "../databases/endeavorDB";
+import { Insertable, Updateable } from "kysely";
+import { Schema } from "express-validator";
 
-export {paramsSchema, createTeacher}
+export { RpcMethodName, rpcMethods };
 
-function createTeacher(teacher: Insertable<Teacher>) {
-    bcrypt.hash(teacher.password, 13, (_, hashedPassword) => {
-        teacher.password = hashedPassword;
-    });
+type RpcMethodName = "createTeacher";
 
-    return endeavorDB
-        .insertInto("teacher")
-        .values(teacher)
-        .returningAll()
-        .executeTakeFirstOrThrow()
-        .also(() => console.log(`Teacher created: ${JSON.stringify(teacher)}`));
-}
-
-export function readTeacher(teacherId: number) {
-}
-
-export function updateTeacher(teacherId: number) {
-}
-
-export function deleteTeacher(teacherId: number) {
-}
-
-const paramsSchema: Record<Method, Schema> = {
-    "createTeacher": {
-        'params.level': {
-            isInt: {
-                options: {min: 1, max: 10},
-                bail: true
-            },
-            errorMessage: 'Invalid level. Level must be an integer between 1 and 20.'
-        },
-        'params.title': {
-            isString: {bail: true},
-            isLength: {
-                options: {min: 1, max: 20}
-            },
-            errorMessage: 'Invalid title. Title must be a non-empty string of max length 20 characters.'
-        },
-        'params.summary': {
-            optional: true,
-            isString: {bail: true},
-            notEmpty: {bail: true},
-            isLength: {
-                options: {min: 1, max: 150}
-            },
-            errorMessage: 'Invalid summary. Summary (if provided) must be a non-empty string of max length 150 characters.'
-        },
-        'params.description': {
-            optional: true,
-            isString: {bail: true},
-            notEmpty: {bail: true},
-            isLength: {
-                options: {min: 1, max: 300}
-            },
-            errorMessage: 'Invalid description. Description (if provided) must be a non-empty string of max length 300 characters.'
-        },
-        'params.thumbnail': {
-            optional: true,
-            isString: {bail: true},
-            notEmpty: {bail: true},
-            errorMessage: 'Invalid thumbnail. Thumbnail (if provided) must be a non-empty string.'
-        }
-    },
-    "readTeacher": {
-        'params.level': {
-            isInt: {
-                options: {min: 1, max: 10},
-                bail: true
-            },
-            errorMessage: 'Invalid level. Level must be an integer between 1 and 20.'
-        },
-        'params.title': {
-            isString: {bail: true},
-            isLength: {
-                options: {min: 1, max: 20}
-            },
-            errorMessage: 'Invalid title. Title must be a non-empty string of max length 20 characters.'
-        },
-        'params.summary': {
-            optional: true,
-            isString: {bail: true},
-            notEmpty: {bail: true},
-            isLength: {
-                options: {min: 1, max: 150}
-            },
-            errorMessage: 'Invalid summary. Summary (if provided) must be a non-empty string of max length 150 characters.'
-        },
-        'params.description': {
-            optional: true,
-            isString: {bail: true},
-            notEmpty: {bail: true},
-            isLength: {
-                options: {min: 1, max: 300}
-            },
-            errorMessage: 'Invalid description. Description (if provided) must be a non-empty string of max length 300 characters.'
-        },
-        'params.thumbnail': {
-            optional: true,
-            isString: {bail: true},
-            notEmpty: {bail: true},
-            errorMessage: 'Invalid thumbnail. Thumbnail (if provided) must be a non-empty string.'
-        }
-    },
+const rpcMethods: Record<RpcMethodName, { rpcMethod: CallableFunction; rpcMethodParamsSchema: Schema }> = {
+  createTeacher: {
+    rpcMethod: createTeacher,
+    rpcMethodParamsSchema: createTeacherParamsSchema(),
+  },
 };
 
-type Method = "createTeacher" | "readTeacher";
+function createTeacher(teacher: Insertable<Teacher>) {
+  bcrypt.hash(teacher.password, 13, (_, hashedPassword) => {
+    teacher.password = hashedPassword;
+  });
+
+  return endeavorDB
+    .insertInto("teacher")
+    .values(teacher)
+    .returningAll()
+    .executeTakeFirstOrThrow()
+    .also(() => console.log(`Teacher created: ${JSON.stringify(teacher)}`));
+}
+
+export function readTeacher(teacherId: number) {}
+
+export function updateTeacher(teacherId: number) {}
+
+export function deleteTeacher(teacherId: number) {}
+
+function createTeacherParamsSchema() {
+  return {
+    "params.level": {
+      isInt: {
+        options: { min: 1, max: 10 },
+        bail: true,
+      },
+      errorMessage: "Invalid level. Level must be an integer between 1 and 20.",
+    },
+    "params.title": {
+      isString: { bail: true },
+      isLength: {
+        options: { min: 1, max: 20 },
+      },
+      errorMessage: "Invalid title. Title must be a non-empty string of max length 20 characters.",
+    },
+    "params.summary": {
+      optional: true,
+      isString: { bail: true },
+      notEmpty: { bail: true },
+      isLength: {
+        options: { min: 1, max: 150 },
+      },
+      errorMessage: "Invalid summary. Summary (if provided) must be a non-empty string of max length 150 characters.",
+    },
+    "params.description": {
+      optional: true,
+      isString: { bail: true },
+      notEmpty: { bail: true },
+      isLength: {
+        options: { min: 1, max: 300 },
+      },
+      errorMessage:
+        "Invalid description. Description (if provided) must be a non-empty string of max length 300 characters.",
+    },
+    "params.thumbnail": {
+      optional: true,
+      isString: { bail: true },
+      notEmpty: { bail: true },
+      errorMessage: "Invalid thumbnail. Thumbnail (if provided) must be a non-empty string.",
+    },
+  };
+}
+
+function readTeacherParamsSchema() {
+  return {
+    "params.level": {
+      isInt: {
+        options: { min: 1, max: 10 },
+        bail: true,
+      },
+      errorMessage: "Invalid level. Level must be an integer between 1 and 20.",
+    },
+    "params.title": {
+      isString: { bail: true },
+      isLength: {
+        options: { min: 1, max: 20 },
+      },
+      errorMessage: "Invalid title. Title must be a non-empty string of max length 20 characters.",
+    },
+    "params.summary": {
+      optional: true,
+      isString: { bail: true },
+      notEmpty: { bail: true },
+      isLength: {
+        options: { min: 1, max: 150 },
+      },
+      errorMessage: "Invalid summary. Summary (if provided) must be a non-empty string of max length 150 characters.",
+    },
+    "params.description": {
+      optional: true,
+      isString: { bail: true },
+      notEmpty: { bail: true },
+      isLength: {
+        options: { min: 1, max: 300 },
+      },
+      errorMessage:
+        "Invalid description. Description (if provided) must be a non-empty string of max length 300 characters.",
+    },
+    "params.thumbnail": {
+      optional: true,
+      isString: { bail: true },
+      notEmpty: { bail: true },
+      errorMessage: "Invalid thumbnail. Thumbnail (if provided) must be a non-empty string.",
+    },
+  };
+}
