f0c4e00110b224a968f0d0004eb0bfeb824eb449
trancongbao <96561274+trancongbao@users.noreply.github.com>
2024-05-28 14:42:35 +0700

    prettier (#57)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import "scope-extensions-js";
-import express from "express";
-import {isAdmin} from "./admin/isAdmin";
-import {isTeacher} from "./teach/isTeacher";
-import {isStudent} from "./study/isStudent";
-import {auth, validateParams} from "./auth/auth";
-import {expressSession} from "./session/session";
-import {validateBody} from "./validation/validation";
-import {admin} from "./admin/admin";
-import {teach} from "./teach/teach";
-import dotenv from "dotenv";
-import cors from "cors"
-import {corsOptions} from "./cors/cors";
+import 'scope-extensions-js'
+import express from 'express'
+import { isAdmin } from './admin/isAdmin'
+import { isTeacher } from './teach/isTeacher'
+import { isStudent } from './study/isStudent'
+import { auth, validateParams } from './auth/auth'
+import { expressSession } from './session/session'
+import { validateBody } from './validation/validation'
+import { admin } from './admin/admin'
+import { teach } from './teach/teach'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { corsOptions } from './cors/cors'
 
-if (process.env.NODE_ENV === "LOCAL") {
-    dotenv.config()
+if (process.env.NODE_ENV === 'LOCAL') {
+  dotenv.config()
 }
 
 const app = express()
@@ -22,7 +22,7 @@ const app = express()
 app.use(cors(corsOptions))
 
 // All apis use a json body (similar to json-rpc)
-app.use(express.json());
+app.use(express.json())
 
 // Session
 app.use(expressSession)
@@ -31,12 +31,12 @@ app.use(expressSession)
 app.use(validateBody)
 
 // Authentication
-app.post("/auth", validateParams, auth);
+app.post('/auth', validateParams, auth)
 
-app.post("/admin", isAdmin, admin);
-app.post("/teach", isTeacher, teach);
-app.post("/study", isStudent);
+app.post('/admin', isAdmin, admin)
+app.post('/teach', isTeacher, teach)
+app.post('/study', isStudent)
 
 app.listen(3000, () => {
-    console.log("Express server started on port 3000.");
-});
+  console.log('Express server started on port 3000.')
+})
