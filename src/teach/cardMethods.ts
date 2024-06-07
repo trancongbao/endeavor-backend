080481b898b6a703444842e74e1d43c1c9d4b715
Tran Cong Bao <trancongbao@gmail.com>
2024-06-07 14:02:24 +0700

    /teach getCards relational

--- src/teach/cardMethods.ts
+++ src/teach/cardMethods.ts
@@ -1,17 +1,20 @@
-import "scope-extensions-js";
-import { endeavorDB } from "../databases/endeavorDB";
-import { Schema } from "express-validator";
-import { sendSuccessResponse } from "../response/success";
-import { Codes, sendErrorResponse } from "../response/error";
-import { encode } from "html-entities";
-import SQL from "sql-template-strings";
-import { query } from "../databases/postgres";
+import 'scope-extensions-js'
+import { endeavorDB } from '../databases/endeavorDB'
+import { Schema } from 'express-validator'
+import { sendSuccessResponse } from '../response/success'
+import { Codes, sendErrorResponse } from '../response/error'
+import { encode } from 'html-entities'
+import SQL from 'sql-template-strings'
+import { query } from '../databases/postgres'
 
-export { RpcMethodName, rpcMethods };
+export { RpcMethodName, rpcMethods }
 
-type RpcMethodName = "createCard" | "addWordsToCard" | "getCards";
+type RpcMethodName = 'createCard' | 'addWordsToCard' | 'getCards'
 
-const rpcMethods: Record<RpcMethodName, { rpcMethod: CallableFunction; rpcMethodParamsSchema: Schema }> = {
+const rpcMethods: Record<
+  RpcMethodName,
+  { rpcMethod: CallableFunction; rpcMethodParamsSchema: Schema }
+> = {
   createCard: {
     rpcMethod: createCard,
     rpcMethodParamsSchema: {},
@@ -24,11 +27,12 @@ const rpcMethods: Record<RpcMethodName, { rpcMethod: CallableFunction; rpcMethod
     rpcMethod: getCards,
     rpcMethodParamsSchema: {},
   },
-};
+}
 
 async function createCard(request: any, response: any) {
-  const teacherUsername = request.session.userInfo.username;
-  const { lesson_id, card_order, front_text, front_audio_uri } = request.body.params;
+  const teacherUsername = request.session.userInfo.username
+  const { lesson_id, card_order, front_text, front_audio_uri } =
+    request.body.params
 
   const sql = SQL`INSERT INTO card (lesson_id, card_order, front_text, front_audio_uri)
             SELECT ${lesson_id}, ${card_order}, ${encode(front_text)}, ${front_audio_uri}
@@ -38,18 +42,22 @@ async function createCard(request: any, response: any) {
                                             INNER JOIN lesson on course.id = lesson.course_id
                                    WHERE teacher_course.teacher_username = ${teacherUsername}
                                      AND lesson.id = ${lesson_id})
-            RETURNING *;`;
+            RETURNING *;`
 
   try {
-    sendSuccessResponse(response, (await query(sql)).rows[0]);
+    sendSuccessResponse(response, (await query(sql)).rows[0])
   } catch (error: any) {
-    sendErrorResponse(response, Codes.RpcMethodInvocationError, error.message);
+    sendErrorResponse(response, Codes.RpcMethodInvocationError, error.message)
   }
 }
 
 async function addWordsToCard(request: any, response: any) {
-  const teacherUsername = request.session.userInfo.username;
-  const { card_id, words }: { card_id: number; words: { id: number; order: number }[] } = request.body.params;
+  const teacherUsername = request.session.userInfo.username
+  const {
+    card_id,
+    words,
+  }: { card_id: number; words: { id: number; order: number }[] } =
+    request.body.params
 
   /**
    * Two queries need to be used.
@@ -69,16 +77,16 @@ async function addWordsToCard(request: any, response: any) {
       INNER JOIN card ON card.lesson_id = lesson.id
       WHERE teacher_course.teacher_username = ${teacherUsername}
       AND card.id = ${card_id};
-    `;
+    `
 
     if ((await query(accessCheckSql)).rows.length === 0) {
       // Teacher does not have access rights, send error response
       sendErrorResponse(
         response,
         Codes.Teach.TeacherPrivilegeMissing,
-        "Teacher does not have access rights to the card."
-      );
-      return;
+        'Teacher does not have access rights to the card.',
+      )
+      return
     }
 
     // Insert words into card_word table
@@ -87,30 +95,32 @@ async function addWordsToCard(request: any, response: any) {
       VALUES 
     `.also((sql) => {
       words.forEach((word, index) => {
-        sql.append(SQL`(${card_id}, ${word.id}, ${word.order})`);
-        index <= words.length - 2 && sql.append(",");
-      });
-      sql.append(SQL`RETURNING *;`);
-    });
+        sql.append(SQL`(${card_id}, ${word.id}, ${word.order})`)
+        index <= words.length - 2 && sql.append(',')
+      })
+      sql.append(SQL`RETURNING *;`)
+    })
 
-    sendSuccessResponse(response, (await query(insertSql)).rows);
+    sendSuccessResponse(response, (await query(insertSql)).rows)
   } catch (error: any) {
-    console.log(error);
-    sendErrorResponse(response, Codes.RpcMethodInvocationError, error.message);
+    console.log(error)
+    sendErrorResponse(response, Codes.RpcMethodInvocationError, error.message)
   }
 }
 
 async function getCards(request: any, response: any) {
-  const teacherUsername = request.session.userInfo.username;
-  const lessonId = request.body.params.id;
+  const teacherUsername = request.session.userInfo.username
+  const { courseId, lessonId } = request.body.params
 
   /**
    * A teacher can only get cards belonging to courses that he is assigned.
    */
   const sql = SQL`
-    SELECT  card.id               as id,
-            card.card_order       as order,
-            card.front_text       as text,
+    SELECT  course.id             as course_id,
+            lesson.id             as lesson_id,
+            card.id               as card_id,
+            card.card_order       as card_order,
+            card.front_text       as card_text,
             word.id               as word_id,
             card_word.word_order  as word_order,
             word.word             as word_word,
@@ -119,40 +129,24 @@ async function getCards(request: any, response: any) {
             word.part_of_speech   as word_part_of_speech,
             word.audio_uri        as word_audio_uri,
             word.image_uri        as word_image_uri
-    FROM card
+    FROM course
+    INNER JOIN lesson ON lesson.course_id = course.id
+    INNER JOIN card ON card.lesson_id = lesson.id
     INNER JOIN card_word ON card_word.card_id = card.id
     INNER JOIN word ON word.id = card_word.word_id
-    WHERE card.lesson_id = ${lessonId}
+    WHERE course.id = ${courseId}
+    AND lesson.id = ${lessonId}
     AND EXISTS  (SELECT 1
                 FROM teacher_course
-                INNER JOIN course ON course.id = teacher_course.course_id
-                INNER JOIN lesson ON lesson.course_id = course.id
                 WHERE teacher_course.teacher_username = ${teacherUsername}
-                AND lesson.id = ${lessonId})
-  `;
+                AND course.id = ${courseId})
+  `
 
   try {
-    const rows = (await query(sql)).rows;
-    const cards: {
-      id: number;
-      order: number;
-      text: string;
-      words: { word_id: number; word_order: number; word_word: string }[];
-    }[] = [];
-
-    rows.forEach((row: any) => {
-      const { id, order, text, ...word } = row;
-      const card = cards.find((card) => card.id === id);
-      if (card) {
-        card.words.push(word);
-      } else {
-        cards.push({ id: id, order: order, text: text, words: [word] });
-      }
-    });
-
-    sendSuccessResponse(response, cards);
+    const rows = (await query(sql)).rows
+    sendSuccessResponse(response, rows)
   } catch (error: any) {
-    console.log(error);
-    sendErrorResponse(response, Codes.RpcMethodInvocationError, error.message);
+    console.log(error)
+    sendErrorResponse(response, Codes.RpcMethodInvocationError, error.message)
   }
 }
